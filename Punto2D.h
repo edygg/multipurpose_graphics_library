9b5969d2f1967a55539412065de86dc13f338fdd
Edilson Gonzalez <edilson.gonza@unitec.edu>
2012-11-24 14:52:16 -0600

    Clase Punto2D

--- Punto2D.h
+++ Punto2D.h
@@ -1,15 +1,15 @@
 #ifndef PUNTO2D_H
 #define PUNTO2D_H
 #include "Figura2D.h"
-#include <Gl/gl.h>
+#include <GL/gl.h>
 
 class Punto2D : public Figura2D {
-	unsigned int x, y;
+	unsigned int cx, cy;
 public:
 	Punto2D(unsigned int, unsigned int);
 	Punto2D(const Punto2D&);
 	virtual ~Punto2D();
-	virtual dibujar();
+	virtual void dibujar();
 	unsigned int getX() const;
 	unsigned int getY() const;
 };

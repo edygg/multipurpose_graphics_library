0105537ac71a6dc442be5f8e2808e0f5ee4dc93c
Edilson Gonzalez <edilson.gonza@unitec.edu>
2012-12-08 19:05:38 -0600

    Prueba comentarios

--- Figura2D.h
+++ Figura2D.h
@@ -1,8 +1,16 @@
 #ifndef FIGURA2D_H
 #define FIGURA2D_H
 
+/*************************************************
+ *				Clase Padre Figura2D 			 *
+ *	Todas las clases heredan de ésta. Posee un	 *
+ *	método dibujar para que cada clase hija 	 *
+ *	sobreescriba y especialice ese método		 *
+ *************************************************/
 class Figura2D {
 public:
+	//Método que será sobreescrito por cada una de
+	//las clases hijas
 	virtual void dibujar() = 0;
 };
 
